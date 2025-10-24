19510bce0d3ede24ffab154bb8316ca14169f74b
Shri Harini Durairaj <shriharinidurairaj@gmail.com>
2025-10-25 00:04:50 +0530

    Final Commmit calc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+function Calculator() {
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+  const ops = ["/", "*", "-", "+", "C"];
+  const updateCalc = (value) => {
+    if (value === "C") {
+      setCalc("");
+      setResult("");
+      return;
+    }
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+
+    setCalc(calc + value);
+  };
+  const calculate = () => {
+    // eslint-disable-next-line no-eval
+    setResult(eval(calc).toString());
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <h1>React Calculator App</h1>
+        <input type="text" className="display" value={calc} disabled />
+        {result ? (
+          <div className="result">{result}</div>
+        ) : (
+          <div className="result"></div>
+        )}
+        {/* <div className="digits">{createDigits()}</div> */}
+        <div className="digits">
+          <button onClick={() => updateCalc("7")}>7</button>
+          <button onClick={() => updateCalc("8")}>8</button>
+          <button onClick={() => updateCalc("9")}>9</button>
+          <button onClick={() => updateCalc("+")}>+</button>
+          <button onClick={() => updateCalc("4")}>4</button>
+          <button onClick={() => updateCalc("5")}>5</button>
+          <button onClick={() => updateCalc("6")}>6</button>
+          <button onClick={() => updateCalc("-")}>-</button>
+          <button onClick={() => updateCalc("1")}>1</button>
+          <button onClick={() => updateCalc("2")}>2</button>
+          <button onClick={() => updateCalc("3")}>3</button>
+          <button onClick={() => updateCalc("*")}>*</button>
+          <button onClick={() => updateCalc("C")}>C</button>
+          <button onClick={() => updateCalc("0")}>0</button>
+          <button onClick={calculate}>=</button>
+          <button onClick={() => updateCalc("/")}>/</button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Calculator;
